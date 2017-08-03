0443401f55198010db60129a4b874c5f37a92197
junxiachen <junxiachen2@github.com>
2017-08-03 11:48:57 +0800

    add toast component

--- src/components/hello.vue
+++ src/components/hello.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>
-    <img src="./../assets/logo.png">
-    hello hello
-  </div>
+    <div>
+        <img src="./../assets/logo.png">
+        hello hello
+        <toast ref="toast"></toast>
+        <button @click="showToast">show Toast</button>
+    </div>
 </template>
 
 <script>
-
+    import Toast from './toast.vue'
+    export default{
+        components: {
+            Toast
+        },
+        methods: {
+            showToast(){
+                this.$refs.toast.show('出错啦~~')
+            }
+        }
+    }
 </script>
 
